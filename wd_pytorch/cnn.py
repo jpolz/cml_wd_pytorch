279cd143f0afb0e2a6b0425cb20a21e2486cd588
Ronja Wackerle <ronja.wackerle@gmx.de>
2024-09-13 11:26:17 +0200

    FC works

--- wd_pytorch/cnn.py
+++ wd_pytorch/cnn.py
@@ -17,7 +17,8 @@ n_filters = [24, 48, 48, 96, 192]
 class cnn_class(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv1d(window,n_filters[0],kernel_size,padding='same')
+        self.conv1a = nn.Conv1d(window,n_filters[0],kernel_size,padding='same')
+        #self.conv1b = nn.Conv1d(window,n_filters[1],kernel_size,padding='same')
         self.act1 = nn.ReLU()
         self.pool1 = nn.MaxPool1d(kernel_size)
         
@@ -36,40 +37,48 @@ class cnn_class(nn.Module):
         self.pool5 = nn.AvgPool1d(kernel_size) #is it global?
 
         ### FC part 
-        self.dense1 = nn.Linear(n_filters[4],n_fc_neurons)
-        self.drop1 = nn.Dropout(dropout)
+        self.dense1 = nn.Linear(2,n_fc_neurons)
+        self.drop1 = nn.Dropout(p=dropout)
         self.dense2 = nn.Linear(n_fc_neurons, n_fc_neurons)
         self.drop2 = nn.Dropout(dropout)
         self.denseOut = nn.Linear(n_fc_neurons, 1)
 
     
     def forward(self, x):
-        x = self.act1(self.conv1(x))
-        print("conv1 finished")
-        #x = self.act1(self.conv1(x))
+        x = self.act1(self.conv1a(x))
+        print("conv1")
+        #x = self.act1(self.conv1b(x))
         x = self.pool1(x)
         print("pool1 finished")
-        
         x = self.act2(self.conv2(x))
         #x = self.act2(self.conv2(x))
         x = self.pool2(x)
+        print("pool2 finished")
         x = self.act3(self.conv3(x))
         #x = self.act3(self.conv3(x))
         x = self.pool3(x)
+        print("pool3 finished")
         x = self.act4(self.conv4(x))
         #x = self.act4(self.conv4(x))
         x = self.pool4(x)
+        print("pool4 finished")
         x = self.act5(self.conv5(x))
         #x = self.act5(self.conv5(x))
         x = self.pool5(x)
-
-        # FC part
-        x = nn.ReLU(self.dense1(x))
+        print("pool5 finished")
+        
+        ### FC part
+        x = nn.ReLU()(self.dense1(x))
+        print("ReLU1 finished")
         x = self.drop1(x)
-        x = nn.ReLU(self.dense2(x))
+        print("first drop")
+        x = nn.ReLU()(self.dense2(x))
+        print("ReLU2 finished")
         x = self.drop2(x)
+        print("second drop")
+        x = nn.Sigmoid()(self.denseOut(x))
 
-        return nn.Sigmoid(self.denseOut(x))
+        return x 
 
 
         
